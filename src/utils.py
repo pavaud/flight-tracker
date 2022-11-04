e6423c46e733db7b0f48b3c15337c302f427f080
Philippe Vaudin <philippe.vaudin@gmail.com>
2022-11-04 23:24:58 +0100

    - added style.css and assets folder - updated app.py with filter div and layout display - refactored flight_tracker() and split into subfunctions

--- src/utils.py
+++ src/utils.py
@@ -492,49 +492,69 @@ def get_routes(dep, arr):
 
     return df
     
+def get_opensky_flights():
+    """ get flyong flights from opensky API """
 
-def flight_tracker():
+    user_name, password = get_credentials()[:2]
 
-    user_name, password, token = get_credentials()
-
-    #Defining the spatial field
+    # defining the spatial field
     lon_min, lat_min = -180., -90.
     lon_max, lat_max = 180., 90.
 
-    #send request to get the current airplane data
+    # send request to get the current airplane data
     url_data = 'https://'+user_name+':'+password+'@opensky-network.org/api/states/all?'+'lamin='+str(lat_min)+'&lomin='+str(lon_min)+'&lamax='+str(lat_max)+'&lomax='+str(lon_max)
     response = requests.get(url_data).json()
 
-    #Load the data as a pandas dataframe
-    col_name = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'long', 'lat', 
+    return response
+
+
+def get_opensky_df(response):
+    """ get dataframe from opensky response """
+
+    # load the data as a pandas dataframe
+    columns = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'long', 'lat', 
                 'baro_altitude', 'on_ground', 'velocity', 'true_track', 'vertical_rate', 'sensors', 
                 'geo_altitude', 'squawk', 'spi', 'position_source']
-    flight_df = pd.DataFrame(response['states'])
+    df = pd.DataFrame(response['states'],columns=columns)
 
-    flight_df = flight_df.loc[:, 0:16]
-    flight_df.columns = col_name
-    #filling missing data
-    flight_df['true_track'] = flight_df['true_track'].fillna(0)
-    flight_df = flight_df.fillna('No Data') 
+    # filling missing data
+    df.true_track = df.true_track.fillna(0)
+    df = df.fillna('NaN')
 
-    #define a list of directictions towards which the plane is heading: North = 0 or 360
-    angles = [flight_df.true_track[i] for i in range(flight_df.true_track.shape[0])]
-
-    df = flight_df.loc[:, ('callsign', 'origin_country', 'time_position', 'long', 'lat', 'baro_altitude', 
-                           'on_ground', 'velocity', 'true_track', 'geo_altitude', 'true_track')] 
-    
     #Define a column of the informetion to be displayed on mouse hovering 
-    df['text'] = 'Call_ID: ' + df['callsign'].astype(str) + ' Origin_Country: ' + df['origin_country'].astype(str) +\
-                 ' Altitude(m): ' + df['baro_altitude'].astype(str) + ' Speed(m/s): ' + df['velocity'].astype(str)
+    df['text'] = 'Callsign: ' + df['callsign'].astype(str) + \
+                 '<br>Origin: ' + df['origin_country'].astype(str) + \
+                 '<br>Altitude (m): ' + df['baro_altitude'].astype(str) + \
+                 '<br>Speed (m/s): ' + df['velocity'].astype(str)
+
+    return df
+
+
+def flight_tracker():
+
+    token = get_credentials()[2]
+    response = get_opensky_flights()
+    df = get_opensky_df(response)
 
     #Define figure and its characteristics
     fig = go.Figure(go.Scattermapbox(
-            lon = df.long, lat = df.lat, text = df.text, mode = 'markers',
-            marker = {'size': 10, 'symbol': 'airport', 'allowoverlap': True, 'angle': angles}
+            lon = df.long, 
+            lat = df.lat, 
+            text = df.text, 
+            mode = 'markers',
+            hoverinfo='none',
+            marker = {'size': 10, 
+                      'symbol': 'airport', 
+                      'allowoverlap': True, 
+                      'angle': df.true_track}
     ))
 
-    fig.update_layout(height = 900, margin = {"r":0, "t":0, "l":0, "b":0},
-                             mapbox = {'accesstoken': token, 'style': "outdoors", 'zoom': 1.9},
-                             showlegend = False)
+    fig.update_layout(height = 900, 
+                      margin = {"r":0, "t":0, "l":0, "b":0},
+                      mapbox = {'accesstoken': token, 
+                                'style': "outdoors", 
+                                'zoom': 1.9},
+                      autosize = True,
+                      showlegend = False)
 
-    return fig
+    return df, fig
