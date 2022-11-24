c7a007de1c952114cfb30061411aeb2c64f0e87c
Philippe Vaudin <philippe.vaudin@gmail.com>
2022-11-24 21:05:45 +0100

    - missed previous commit

--- src/utils.py
+++ src/utils.py
@@ -736,7 +736,7 @@ def get_opensky_flights():
     url_data = 'https://'+user_name+':'+password+'@opensky-network.org/api/states/all?'+'lamin='+str(lat_min)+'&lomin='+str(lon_min)+'&lamax='+str(lat_max)+'&lomax='+str(lon_max)
     response = requests.get(url_data).json()
 
-    update_position(response)
+    #update_position(response)
 
     return response
 
